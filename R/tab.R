d1032d55ec96b0ce815f2592d4de5d001c6ca536
JIANHONG OU <jianhong@users.noreply.github.com>
2022-07-27 15:30:03 -0400

    update the algorithm of sortLevels

--- R/tab.R
+++ R/tab.R
@@ -13,11 +13,18 @@ sortLevels <- function(lev, controls=c("control", "wildtype", "wt", "cont", "con
   ml <- min(which(ml!=1))
   if(ml>0){
     l1.sub <- paste(unlist(l1.sub)[seq.int(ml-1)], collapse="")
-    l1.sub <- sub(l1.sub, "", l1)
-    l1.sub <- suppressWarnings(as.numeric(l1.sub))
-    if(all(!is.na(l1.sub))){
-      l1 <- l1[order(l1.sub)]
-    }
+    l1.sub0 <- sub(l1.sub, "", l1)
+  }else{
+    l1.sub0 <- l1
+  }
+  ## postfix
+  l1.sub <- strsplit(l1.sub0, split="")
+  l1.sub <- Reduce(intersect, l1.sub)
+  l1.sub <- paste(l1.sub, collapse = "")
+  l1.sub <- sub(l1.sub, "", l1.sub0)
+  l1.sub <- suppressWarnings(as.numeric(l1.sub))
+  if(all(!is.na(l1.sub))){
+    l1 <- l1[order(l1.sub)]
   }
   c(l0, l1)
 }

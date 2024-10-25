b9d2efc0813ee3a759d9590c99d641c213c90c64
JIANHONG OU <jianhong@users.noreply.github.com>
2024-10-25 14:32:12 -0400

    try to debug for remote server.

--- R/scDRatac.R
+++ R/scDRatac.R
@@ -24,6 +24,7 @@ scDRatac <- function(
         plotAspectRatio,
         keepXYlables,
         ...) {
+    saveRDS(as.list(environment()), 'www/tmp.rds')
     if (gene1[1] == "") {
         return(ggplot())
     }
